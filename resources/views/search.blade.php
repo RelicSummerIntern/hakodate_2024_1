8218d0a2cf1e124a9b55a96a694c8307b746cca0
Su1Be1So1De1PLaTa1 <lunadial965@gmail.com>
2024-08-22 15:50:50 +0900

    店舗詳細画面

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -52,6 +52,20 @@
             </div>
         </div>
     </div>
+
+    <!-- 他のコンテンツの下に追加 -->
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-center">
+                <!-- 戻るボタン -->
+                <button onclick="history.back()" class="bg-gray-500 text-white py-2 px-4 rounded">
+                    戻る
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
 </x-app-layout>
 
 
