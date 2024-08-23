ca8ac1cac1508c46fde3557d1eee5c1482fe0498
yuzukiimai <imai.yuzuki.s@gmail.com>
2024-08-23 10:24:34 +0900

    写真表示

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,15 +26,4 @@
             </div>
         </div>
     </div>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <a href="{{ route('myposts') }}" class="bg-white border-b border-gray-200 p-6 block w-full text-center
-                font-semibold text-gray-800 hover:bg-gray-100 text-decoration-none">
-                    自分の投稿を確認する
-                </a>
-            </div>
-        </div>
-    </div>
 </x-app-layout>
